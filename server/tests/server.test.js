8bf65d3e9153dcfa4fe969e3160d2e28940c7223
Denham Coote <denhamcoote@gmail.com>
2017-02-09 15:54:43 +0000

    Add POST /users/login test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -213,7 +213,7 @@ describe('POST /users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -228,12 +228,67 @@ describe('POST /users', () => {
   });
 
   it('should not create user if email in use', (done) => {
-    var email = 'denham@example.com'
+    var email = 'denhamcoote@gmail.com'
     var password = '123mnb!'
     request(app)
       .post('/users')
-      .send({email, password})
+      .send({
+        email: users[0].email,
+        password: 'Password123'
+      })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.header['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'invalid'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+
   });
 });
